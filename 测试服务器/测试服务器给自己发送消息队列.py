490d7e998d7ecb6506bbabccf1e098837468f2a7
dagege1993 <18273711329@163.com>
2018-10-10 14:44:56 +0800

    pymongo_数据查询

--- 测试服务器/测试服务器给自己发送消息队列.py
+++ 测试服务器/测试服务器给自己发送消息队列.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 import pika
 
 # 生产者代码
@@ -8,26 +9,44 @@ import pika
 # chan = s_conn.channel()  # 在连接上创建一个频道
 
 # import pika
+
+# 本地可以连接MQ
 hostname = '10.90.60.10'
 port = 5672
 virtual_host = '/'
 credentials = pika.PlainCredentials(username='bigdata', password='bigdata')
 parameters = pika.ConnectionParameters(host=hostname, port=port, virtual_host=virtual_host)
 connection = pika.BlockingConnection(parameters)
-
 channel = connection.channel()
 
+# 测试服务器
+# hostname = '192.168.109.22'
+# port = 5672
+# virtual_host = '/'
+# credentials = pika.PlainCredentials(username='bigdata', password='bigdata')
+# parameters = pika.ConnectionParameters(host=hostname, port=port, virtual_host=virtual_host, credentials=credentials)
+# connection = pika.BlockingConnection(parameters)
+# channel = connection.channel()
+
 # 声明queue,消息将在这个队列中进行传递,如果队列不存在,则创建
-channel.queue_declare(queue='loanRiskModelFromPython', durable=True)
+channel.queue_declare(queue='riskModel4PythonAddition', durable=True)
 
 # exchange-- 它使我们能够确定指定消息应该到哪个队列去,
 # 向队列插入数值 rounting_key 是队列名,Body是要插入的内容
-body = {'platformId': '20000004', 'customerOpenId': 'c1852d65eec745d728a84de38b08293a', 'riskId': '123',
-        'entName': '灵宝市大旺副食商行', 'creditCode': '92411282MA40L9CL1J', 'startDate': '2018-07-06', 'endDate': '2018-07-07',
-        'accessToken': 'f3f0597f-c8f2-4337-9c75-1006dac19924', 'refreshToken': '81ba0cc7-d835-494e-a028-b6cc91597b95'}
+body = {
+	'platformId': '60000001',
+	'customerOpenId': 'b75b14255acd18a05b60b71290478277',
+	'riskId': 'fb358c7443644219948f06d9db8ca683',
+	'entName': '洛阳市站区新大华超市',
+	'creditCode': '92411282MA40L9CL1J',
+	'startDate': '2018-08-21',
+	'endDate': '2018-10-07',
+	'accessType': '4',
+	'accessToken': '5d9f8ad2-c088-4536-bce0-f3f51009a0d2'
+}
 
 channel.basic_publish(exchange='',
-                      routing_key='loanRiskModel4Python',
+                      routing_key='riskModel4PythonAddition',
                       body=str(body))
-print("[x] Sent ‘hello word!‘")
+print('[x] Sent ‘hello word!‘')
 connection.close()
