5c25a7eb850dc6778b76f40d7801c49cadd5cede
dagege1993 <18273711329@163.com>
2018-10-12 11:18:39 +0800

    pymongo数据库查询且删除

--- python数据库/pymongo查找数据并且删除.py
+++ python数据库/pymongo查找数据并且删除.py
@@ -7,7 +7,8 @@ db_auth.authenticate("jkspider", "adminadmin")
 db = client['jkspider']['duolabao']
 
 # queryArgs = {'entName': '苏州食永兴餐饮有限公司', 'shop_id': '374eba3e3206deef24ff05471c96286c|c7d5c6fb686c477486615e84410bfc1e'}
-queryArgs = {'entName': '北京竹悦阁美容服务有限公司', 'shop_id': '431202e219d1c1970a29e9e4709ac562|682d7b9cdb7c468ea6ab4fa297c1231d'}
+# queryArgs = {'entName': '北京竹悦阁美容服务有限公司', 'shop_id': '431202e219d1c1970a29e9e4709ac562|682d7b9cdb7c468ea6ab4fa297c1231d'}
+queryArgs = {'entName': '北京豪仕强商贸有限公司', 'shop_id': '3c9f3bde48fd72b64f4b2adb4fbc1787|c39b539d1369416b9e7a4d60e4d5fb99'}
 search_res = db.find(queryArgs)
 
 # 统计数据用
@@ -20,16 +21,16 @@ print(i)
 # client = MongoClient(host='localhost', port=27017)
 # db_auth = client.admin
 # db = client['localhost_duolabao']['jianfeng']
-
-
-# queryArgs = {'entName': '北京竹悦阁美容服务有限公司'}
-
-# search_res = db.find({"entName": "郑州市金水区董氏兄弟水果超市"}).sort([{"completeTime": -1}])
+#
+#
+# queryArgs = {'entName': '苏州食永兴餐饮有限公司'}
+# # queryArgs = {'entName': '北京竹悦阁美容服务有限公司'}
+#
 # search_res = db.find(queryArgs)
 # print(search_res)
 # for result in search_res:
 # 	print(result)
-# db.delete_one(result)  #逐条删除
+# 	db.delete_one(result)  #逐条删除
 
 
 
