08b794a8445c96293ec3f4619f6348652c711859
dagege1993 <18273711329@163.com>
2018-11-01 11:13:18 +0800

    python列表包含字典转str用join方法是不行的.只能json的dumps转为字符串,为了转中文需要设置ensure_ascii=False,

---
+++ python字符串处理/列表包含字典元素转字符串.py
@@ -0,0 +1,8 @@
+import json
+
+ll = [{'count': 180, 'tag': '味道赞'}, {'count': 145, 'tag': '服务热情'}, {'count': 118, 'tag': '点心好'},
+      {'count': 35, 'tag': '价格实惠'}, {'count': 33, 'tag': '回头客'}, {'count': 15, 'tag': '干净卫生'},
+      {'count': 6, 'tag': '饮品赞'}, {'count': 5, 'tag': '菜品健康'}, {'count': 4, 'tag': '现做现卖'}, {'count': 3, 'tag': '朋友聚餐'}]
+print(type(json.dumps(ll, ensure_ascii=False, )))
+
+# 这样会报错,因为join方法期望是列表里包含全字符串而不是字典
